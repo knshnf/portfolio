40d8636521556d44440edd0c457bc6f2ff12bd05
kenshin11514 <kenshin_fetalvero@dlsu.edu.ph>
2024-01-24 01:15:05 +0800

    Add dynamic rendering of projects from JSON

---
+++ script.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    $.getJSON('info.json', function(data) {
+        $.each(data.projects, function(i, project) {
+            var projectSection = "<section>" +
+                "<h3>" + project.name + "</h3>" +
+                "<p>" + project.description;
+
+            if (project.link !== "") {
+                projectSection += " <a href=\"" + project.link + "\"> GitHub </a>";
+            }
+
+            projectSection += "</p>";
+
+            $.each(project.tags, function(i, tag) {
+                projectSection += "<span>" + tag + "</span>";
+            });
+
+            projectSection += "</section>";
+            $("#projects-container").append(projectSection);
+        });
+    });
+});
